335eb6846699e4e7477c55a422edf7e04736483b
ajromen <109547906+ajromen@users.noreply.github.com>
2025-01-13 17:58:48 +0100

    front

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Contact } from '../Models/conctact.model';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HttpClientModule, AsyncPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Phonebook';
+  http=inject(HttpClient);
+
+  contacts$ = this.getContacts();
+
+  private getContacts() :Observable<Contact[]>{
+    return this.http.get<Contact[]>('http://localhost:5225/api/Contacts');
+  }
 }
+
